4f4161f28ac961141a4ab9e2544da9911838fbb3
TheHadiAhmadi <TheHadiAhmadi@gmail.com>
2022-03-03 20:47:18 +0430

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import adapter from "svelte-adapter-deno-deploy";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      out: "dist",
+    }),
   },
 };
 
